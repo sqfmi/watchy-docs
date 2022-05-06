3f52f2338995106e911c221bcab1c5aff61590a2
Alexandre J. Raymond <alexandre.j.raymond@gmail.com>
2022-05-05 20:53:26 -0400

    Fix link to GitHub 3d rendering API

--- src/pages/watchcases/WatchCaseSTL.js
+++ src/pages/watchcases/WatchCaseSTL.js
@@ -7,7 +7,7 @@ class WatchCaseSTL extends Component {
         return(
         <div className={'col col--6 '+watchcase.case}>
             <h1>{this.props.title}</h1>
-            <iframe height='420' width='420' frameBorder='0' src={'https://render.githubusercontent.com/view/3d?url=https://raw.githubusercontent.com/sqfmi/watchy-cases/master/'+this.props.path} title={this.props.title}></iframe>
+            <iframe height='420' width='420' frameBorder='0' src={'https://viewscreen.githubusercontent.com/view/solid?url=https://raw.githubusercontent.com/sqfmi/watchy-cases/master/'+this.props.path} title={this.props.title}></iframe>
             <a className={'button button--outline button--info'} href={'https://github.com/sqfmi/watchy-cases/tree/main/' + this.props.title} target="_blank">GitHub</a>
         </div>
         )
